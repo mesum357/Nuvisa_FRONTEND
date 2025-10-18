3914e391129f19b1d4a00b122faa3d540b36ce4e
Babar Awan <awanb036@gmail.com>
2025-10-18 18:25:34 +0500

    enable reviews

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ const Index = () => {
             />
           </div>
           <VisaHeroSection />
-          {/* <Reviews /> */}
+           <Reviews /> 
         </main>
         <CountryCardsSection />
 
