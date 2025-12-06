517050eee8eeec0984504ef34de36176d273f7f9
Moeen19 <moeen12yt@gmail.com>
2025-12-07 00:51:16 +0500

    manifest fix

--- src/store/redux-provider.tsx
+++ src/store/redux-provider.tsx
@@ -1,7 +1,7 @@
+import React, { useEffect } from "react";
 import { Provider, useDispatch } from "react-redux";
 import { store } from "./index";
 import { persistStore } from "redux-persist";
-import { useEffect } from "react";
 import { setAuthState } from "./authSlice";
 
 persistStore(store);
