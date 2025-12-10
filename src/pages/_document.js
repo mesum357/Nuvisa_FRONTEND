7d234fd826eb90967b8d4378c75da98bb32562d0
Moeen19 <moeen12yt@gmail.com>
2025-12-10 23:35:35 +0500

    document js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -66,21 +66,21 @@ export default function Document() {
         </noscript>
 
         {/* ===== FAVICONS ===== */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
         
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
 
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
 
-        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png?v=2" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v=2" />
 
-        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png?v=2" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png?v=2" />
 
-        <link rel="apple-touch-icon" href="/favicon-192x192.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/favicon-192x192.png?v=2" />
+        <link rel="manifest" href="/manifest.json?v=2" />
 
 
         {/* ===== META / SOCIAL ===== */}
